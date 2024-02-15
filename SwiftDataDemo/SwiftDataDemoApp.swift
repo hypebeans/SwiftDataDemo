a92f63365ca2589b9162a9c58342fb078b379f1c
hypebeans <24372255+hypebeans@users.noreply.github.com>
2024-02-15 21:16:30 +0900

    Initial Commit

---
+++ SwiftDataDemo/SwiftDataDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataDemoApp.swift
+//  SwiftDataDemo
+//
+//  Created by Jonathan Budiman on 2024/02/15.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
